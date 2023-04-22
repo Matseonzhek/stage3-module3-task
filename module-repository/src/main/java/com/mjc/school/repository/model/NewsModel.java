9cd6c15e82a4806bc0c8ea2d7f9d4838f3e2485e
Tanya Matseonzhek <matseonzhek@gmail.com>
2023-04-22 20:57:46 +0400

    moved BaseEntity to /model

--- module-repository/src/main/java/com/mjc/school/repository/model/NewsModel.java
+++ module-repository/src/main/java/com/mjc/school/repository/model/NewsModel.java
@@ -1,6 +1,5 @@
 package com.mjc.school.repository.model;
 
-import com.mjc.school.repository.BaseEntity;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
